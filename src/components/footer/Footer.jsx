147be59b4a0b44483bf3057dae678c00388b21e5
Sambit Behera <beherasambit907@gmail.com>
2025-10-24 16:51:51 +0530

    modification commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,6 +3,7 @@ import FooterList from "./footerIterator/FooterList";
 import LowerFooter from "./footerIterator/LowerFooter";
 import MidFooter from "./footerIterator/MidFooter";
 import { motion, useInView } from "framer-motion";
+import QuoteModal from "../pages/QuoteModal";
 
 const Footer = () => {
   const [open, setOpen] = useState(false)
@@ -37,8 +38,9 @@ const Footer = () => {
           <button className="px-6 py-3 sm:px-7 sm:py-4 text-[#0059FF] text-base sm:text-lg lg:text-xl leading-[100%] font-sfpro font-semibold bg-white rounded-full whitespace-nowrap">
             Talk to Expert
           </button>
-          <span className="px-6 py-3 sm:px-8 sm:py-4 border border-white text-base sm:text-lg lg:text-xl leading-[100%] text-[#FFFFFF] font-sfpro font-bold rounded-full whitespace-nowrap">
+          <span className="px-6 py-3 sm:px-8 sm:py-4 border border-white text-base sm:text-lg lg:text-xl leading-[100%] text-[#FFFFFF] font-sfpro font-bold rounded-full whitespace-nowrap" onClick={()=>setOpen((prev)=>!prev)}>
             Get a quote
+            {open && (<QuoteModal onClose={()=>setOpen(false)} />)}
           </span>
         </div>
       </motion.div>
